ec8702550680c932701e9126caa138852fd15774
huyhung <hunghh@coin98.finance>
2023-10-27 15:03:37 +0700

    chore: img banner 11

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "378b4b63f52ae2b6"; }
+/******/ 		__webpack_require__.h = function() { return "b3e48479f8737ae9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
