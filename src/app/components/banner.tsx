9e20d286314ec3680d807e9c67547bf07e01948c
huyhung <hunghh@coin98.finance>
2023-10-27 14:19:28 +0700

    chore: css mobile

--- src/app/components/banner.tsx
+++ src/app/components/banner.tsx
@@ -21,9 +21,13 @@ export default function Banner() {
 
     return (
         <div className='relative'>
-            <img className='h-screen w-screen object-center object-cover' src='/DSC06624-2.jpg' alt='banner'/>
+            <img className='sm:hidden h-screen w-screen object-center object-cover'
+                 src='/DSC06624-1.jpg'
+                 alt='banner'/>
+            <img className='hidden sm:block h-screen w-screen object-center object-cover' src='/DSC06624-2.jpg'
+                 alt='banner'/>
             <div
-                className="z-20 flex-col absolute bottom-16 left-0 flex w-full items-center justify-center gap-7 font-mono text-sm">
+                className="z-40 flex-col absolute bottom-16 left-0 flex w-full items-center justify-center gap-7 font-mono text-sm">
                     <span
                         className='sm:block hidden text-white text-center text-xl'>{`🇼‌🇪‌🇩‌🇩‌🇮‌🇳‌🇬‌ 🇮‌🇳‌🇻‌🇮‌🇹‌🇦‌🇹‌🇮‌🇴‌🇳‌`}</span>
                 <span
