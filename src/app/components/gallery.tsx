d7ba726440bf47c658f16cf47adb6bf403cdac64
huyhung <hunghh@coin98.finance>
2023-11-08 11:37:14 +0700

    chore: update location

--- src/app/components/gallery.tsx
+++ src/app/components/gallery.tsx
@@ -106,7 +106,7 @@ export default function Gallery() {
                             key={index}
                             src={image.thumbnail}
                             alt={`Thumbnail ${index + 1}`}
-                            className={`${selectedImage === image.original && 'opacity-100'} opacity-30 h-[40px] xl:h-[50px] object-center object-cover rounded`}
+                            className={`${selectedImage === image.original ? 'opacity-100' : 'opacity-30'} h-[40px] xl:h-[50px] object-center object-cover rounded`}
                             onClick={() => changeImage(image.original)}
                         />
                     ))}
